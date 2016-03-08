d5531e0d926fe87da8d5aefd6257679710a0a85c
Soren Telfer <soren.telfer@gmail.com>
2016-03-07 23:51:53 -0800

    Changed some attributes around

--- include/kernel/strtox.h
+++ include/kernel/strtox.h
@@ -1,8 +1,11 @@
 #ifndef _STRTOX_H
 #define _STRTOX_H
 
-extern int __attribute__((warn_unused_result)) strtoul(const char *s, unsigned int base, unsigned long *res);
-extern int __attribute__((warn_unused_result)) strtol(const char *s, unsigned int base, long *res);
+#include <kernel/compiler.h>
+
+
+extern int __must_check strtoul(const char *s, unsigned int base, unsigned long *res);
+extern int __must_check strtol(const char *s, unsigned int base, long *res);
 
 #define STRTOX_OVERFLOW (1 << (sizeof(unsigned long) - 1))
 
