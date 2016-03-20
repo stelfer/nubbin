4b4f9e2245bc78bef9a77c74b8989f8c94211297
Soren Telfer <soren.telfer@gmail.com>
2016-03-20 10:04:38 -0700

    Added auto newline and scrolling

--- kernel/asm/print_string.asm
+++ kernel/asm/print_string.asm
@@ -10,5 +10,16 @@ print_string_loop:
         inc bx
         jmp print_string_loop
 print_string_done:   
+	;; Move to the next line
+	mov ah, 0x03
+	mov bh, 0
+	int 0x10
+	mov dl, 0
+	inc dh
+	mov ah, 0x02
+	int 0x10
+	mov ah, 0x07
+	mov al, 1
+	int 0x10
         popa
         ret
