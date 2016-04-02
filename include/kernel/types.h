341a6bd0ae2d8337206563a65a7acb2008b37333
Soren Telfer <soren.telfer@gmail.com>
2016-04-01 17:15:19 -0700

    Added uint64_t

--- include/kernel/types.h
+++ include/kernel/types.h
@@ -1,3 +1,5 @@
+// Copyright (C) 2016 by Soren Telfer - MIT License. See LICENSE.txt
+
 #ifndef _TYPES_H
 #define _TYPES_H
 
@@ -9,6 +11,9 @@ typedef __INT16_TYPE__ int16_t;
 
 typedef __UINT32_TYPE__ uint32_t;
 typedef __INT32_TYPE__ int32_t;
+
+typedef __UINT64_TYPE__ uint64_t;
+typedef __INT64_TYPE__ int64_t;
 
 typedef __SIZE_TYPE__ size_t;
 
@@ -30,5 +35,4 @@ typedef unsigned long long llu_t;
 #error
 #endif
 
-#endif  /* _TYPES_H */
-
+#endif /* _TYPES_H */
